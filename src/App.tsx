9606adbca68da0438fae3774f6562a0f13aa392e
burnh <mai907507@gnail.com>
2024-04-13 00:47:49 +0700

    Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const travelItem: {
+  image: string;
+  name: string;
+  totalReviews: number;
+  rating: number;
+  location: string;
+  price: number;
+  date: string;
+  departure: string;
+  features: {
+    freeWifi: boolean;
+    freePacking: boolean;
+    specialOffer: boolean;
+  };
+}[] = [
+  {
+    image: "link",
+    name: "con cac",
+    totalReviews: 1,
+    rating: 1,
+    location: "nha tao",
+    price: 1,
+    date: "14/8.2003",
+    departure: "cc",
+    features: { freePacking: true, freeWifi: false, specialOffer: false },
+  },
+];
+console.log("🚀 ~ travelItem:", travelItem);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline text-red-900">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-red-900">
+        World: Lo Con cac I Have a BoyFriend !
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
