d850498b293c60dfe05f7439955e497725d843ea
burnh <mai907507@gnail.com>
2024-04-16 23:15:11 +0700

    Todo App with TS and useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
+import TodoApp from "./components/TodoApp";
 
-import "./App.css";
-
-const travelItem: {
-  image: string;
-  name: string;
-  totalReviews: number;
-  rating: number;
-  location: string;
-  price: number;
-  date: string;
-  departure: string;
-  features: {
-    freeWifi: boolean;
-    freePacking: boolean;
-    specialOffer: boolean;
-  };
-}[] = [
-  {
-    image: "link",
-    name: "con cac",
-    totalReviews: 1,
-    rating: 1,
-    location: "nha tao",
-    price: 1,
-    date: "14/8.2003",
-    departure: "cc",
-    features: { freePacking: true, freeWifi: false, specialOffer: false },
-  },
-];
-console.log("🚀 ~ travelItem:", travelItem);
-
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        World: Lo Con cac I Have a BoyFriend !
-      </h1>
+      <TodoApp></TodoApp>
     </>
   );
-}
+};
 
 export default App;
