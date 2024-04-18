77099ec3b64002c1bb2c3b5e019bb09a5c3bdedf
burnh <mai907507@gnail.com>
2024-04-18 19:11:44 +0700

    set up colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,25 @@ export default {
   theme: {
     extend: {},
     colors: {
-      primary: ""
+      primary: '#1DC071',
+      seconday: '#6F49FD',
+      text1: '#171725',
+      text2: '#4B5264',
+      text3: '#808191',
+      text4: '#B2B3BD',
+      iconColor: '#A2A2A8',
+      white: '#FFFFFF',
+      whiteSoft: '#FCFBFF',
+      graySoft: '#FCFCFC',
+      stock: '#F1F1F3',
+      lite: '#FCFCFD',
+      error: '#EB5757',
+      darkbg: '#13131A',
+      darkSecondary: '#1C1C24',
+      softDark: '#22222C',
+      darkSoft: '#24242C',
+      darkStroke: '#3A3A43',
+      darkRed: '#422C32'
     }
   },
   plugins: [],
